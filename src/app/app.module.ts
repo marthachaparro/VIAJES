bb9364ced9c9615f19138335d027263959e1243c
marthachaparro <martha.chaparro5@misena.edu.co>
2023-07-11 14:02:09 -0500

    2.0 version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './estructura/header/header.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { NavComponent } from './estructura/nav/nav.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+import { UsuarioComponent } from './modulos/usuario/usuario.component';
+//import { HeaderComponent } from './estructura/header/header.component';
+//import { NavComponent } from './estructura/nav/nav.component';
+//import { FooterComponent } from './estructura/footer/footer.component';
+//import { PrincipalComponent } from './modulos/principal.component';
+//import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+//import { LoginComponent } from './modulos/login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    LoginComponent,
+    UsuarioComponent,
+    //HeaderComponent,
+   // NavComponent,
+   // FooterComponent,
+    //PrincipalComponent,
+  //  DashboardComponent,
+   // LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
